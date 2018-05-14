8ca236a83ced6db2f9ac7131597f21d0bed6a520
ikikika <iki.jito@gmail.com>
2018-05-14 15:32:52 +0800

    create model for items and export it so that we can use it in other files

---
+++ models/Item.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose'); //bring in mongoose
+const schema = mongoose.Schema;
+
+//create Schema (the fields that we want)
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// export model so that we can bring into other files
+module.exports = Item = mongoose.model('item', ItemSchema);
