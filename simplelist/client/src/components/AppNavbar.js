332ffc63f8a4dc5e0ad223f97e5262af24f904b6
ikikika <iki.jito@gmail.com>
2018-05-16 16:25:09 +0800

    create navbar and import into app.js

--- simplelist/client/src/components/AppNavbar.js
+++ simplelist/client/src/components/AppNavbar.js
@@ -11,7 +11,38 @@ import {
 } from 'reactstrap';
 
 class AppNavbar extends Component {
-  
+  state = {
+    isOpen: false
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">MERN List</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://google.com">Google</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
 }
+
+
+
 
 export default AppNavbar;
