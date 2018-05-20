700cf44b4925412e70b6b38c1e492e680c51b180
ikikika <iki.jito@gmail.com>
2018-05-20 12:55:32 +0800

    delete item from db

--- simplelist/client/src/actions/itemActions.js
+++ simplelist/client/src/actions/itemActions.js
@@ -25,11 +25,14 @@ export const addItem = (item) => dispatch => {
     )
 };
 
-export const deleteItem = (id) => {
-  return {
-    type: DELETE_ITEM, //this is the action
-    payload: id
-  };
+export const deleteItem = (id) => dispatch => {
+  axios.delete(`/api/items/${id}`)
+    .then(res=>
+      dispatch({
+        type: DELETE_ITEM,
+        payload: id
+      })
+    )
 };
 
 
