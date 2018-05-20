aa7380939a0387d1dd6c21bb34880c65d02be58d
ikikika <iki.jito@gmail.com>
2018-05-20 13:22:00 +0800

    prepare for deployment to heroku

--- simplelist/server.js
+++ simplelist/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser"); //take request and get data from body
+const path = require('path');
 
 const items = require('./routes/api/items'); // bring in item api route
 
@@ -23,8 +24,19 @@ mongoose.connect(db)
   );
 
 // use routes
-  app.use('/api/items', items); //anything that goes to /api/items will go to the items const which will refer to the file routes/api/items
+app.use('/api/items', items); //anything that goes to /api/items will go to the items const which will refer to the file routes/api/items
 
-const port = process.env.port || 5000;
+//serve static assets if in production
+if( process.env.NODE_ENV === 'production' ){
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
