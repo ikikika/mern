78cb4c2fd1ad40453838f28314cd10b9172116d9
ikikika <iki.jito@gmail.com>
2018-05-21 16:59:46 +0800

    devconn connect to mongodb

---
+++ devconnector/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://devconn:Gbr4uBvhzXu2@ds211592.mlab.com:11592/devconnector'
+}
--- devconnector/server.js
+++ devconnector/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//db config
+const db = require('./config/keys').mongoURI;
+
+//connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log('mongodb connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello 2345'));
 
