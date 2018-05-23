8580a2f34500c8d2b630bab8c4e86c5b4e386bd4
ikikika <iki.jito@gmail.com>
2018-05-23 16:13:24 +0800

    devconn creating jwt

--- devconnector/config/keys.js
+++ devconnector/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://devconn:Gbr4uBvhzXu2@ds211592.mlab.com:11592/devconnector'
-}
+  mongoURI: 'mongodb://devconn:Gbr4uBvhzXu2@ds211592.mlab.com:11592/devconnector',
+  secretOrKey: 'secret'
+};
