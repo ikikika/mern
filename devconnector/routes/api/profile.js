0b8f1b9ca48bc9a748621c07df5a29559115394f
ikikika <iki.jito@gmail.com>
2018-05-30 15:05:22 +0800

    devconn profile route

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
+//load profile model
+const Profile = require('../../models/Profile');
+
+//load profile model
+const User = require('../../models/User');
+
+// @route   GET api/users/test
+// @desc    Tests profile  route
+// @access  Public
 router.get('/test', (req, res) => res.json({msg: "profile work"})); //this will point to /api/users/test
+
+// @route   GET api/profile
+// @desc    get current users profile
+// @access  Private
+
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+const errors = {};
+  Profile.findOne({ user: req.user.id })
+    .then( profile => {
+      if(!profile){
+        errors.noprofile = "there is no profile for this user";
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 
 module.exports = router;
