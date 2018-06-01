4a86fe8c1a2ae812ad659133395f088903e4fa61
ikikika <iki.jito@gmail.com>
2018-06-01 18:51:21 +0800

    devconn populate user object in profile to show avatar

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -24,6 +24,7 @@ router.get('/test', (req, res) => res.json({msg: "profile work"})); //this will 
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
 const errors = {};
   Profile.findOne({ user: req.user.id })
+    .populate('user', ['name', 'avatar'])
     .then( profile => {
       if(!profile){
         errors.noprofile = "there is no profile for this user";
