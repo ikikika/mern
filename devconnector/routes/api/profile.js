9446c5094314f865180938b59fd07a20f642c5a8
ikikika <iki.jito@gmail.com>
2018-06-02 19:08:03 +0800

    devconn add experience route

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -98,5 +98,27 @@ router.post('/', passport.authenticate('jwt', { session: false }),
       });
 });
 
+// @route   POST api/profile/experience
+// @desc    add experience to profile
+// @access  Private
+
+router.post('/experience', passport.authenticate('jwt', {session:false}), (req, res) => {
+  Profile.findOne({ user: req.user.id})
+    .then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.bpdy.description
+      }
+      //add to experience array
+      profile.experience.unshift(newExp);
+      profile.save()
+        .then(profile=> res.json(profile));
+    });
+});
 
 module.exports = router;
