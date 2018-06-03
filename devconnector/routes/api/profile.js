7dd1d521724ac6cf2b1828f0c9527b27f14a8afb
ikikika <iki.jito@gmail.com>
2018-06-03 18:40:34 +0800

    devconn delete edu route & save to db

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -194,4 +194,33 @@ router.delete(
 
 });
 
+// @route   DELETE api/profile/education/:edu_id
+// @desc    delete education
+// @access  Private
+
+router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', {session:false}),
+  (req, res) => {
+
+  Profile.findOne( {user: req.user.id} )
+    .then( profile => {
+      // get remove index
+      const removeIndex = profile.education
+        .map(item => item.id)
+        .indexOf(req.params.edu_id);
+
+        if( removeIndex >= 0 ){
+          // splice out of array
+         profile.education.splice(removeIndex, 1);
+          // //save
+          profile.save().then( profile => res.json(profile) );
+        }
+
+    })
+    .catch(err => res.status(404).json(err));
+
+});
+
+
 module.exports = router;
