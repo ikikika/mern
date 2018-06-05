997e7c4e90d89e615ffcab6aff8a8f6cc0429d26
ikikika <iki.jito@gmail.com>
2018-06-05 12:42:33 +0800

    devconn create post route

--- devconnector/routes/api/posts.js
+++ devconnector/routes/api/posts.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
+const Post = require('../../models/Post');
+
+// @route   GET api/posts/test
+// @desc    Tests post  route
+// @access  Public
 router.get('/test', (req, res) => res.json({msg: "posts work"})); //this will point to /api/users/test
+
+// @route   POST api/posts/
+// @desc    Create post
+// @access  Private
+router.post('/', passport.authenticate('jwt', {session:false}), (req,res)=>{
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.name,
+    user: req.user.id
+  });
+
+  newPost.save().then(post=>res.json(post));
+});
+
 
 module.exports = router;
