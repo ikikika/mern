aa1c14780fcf5bebd1d281796b9a7a7f71384fb5
ikikika <iki.jito@gmail.com>
2018-06-07 15:46:46 +0800

    devconn like post

--- devconnector/routes/api/posts.js
+++ devconnector/routes/api/posts.js
@@ -76,4 +76,28 @@ router.delete('/:id', passport.authenticate('jwt', {session:false}), (req,res)=>
     });
 });
 
+// @route   api/posts/like/:id
+// @desc    like post
+// @access  Private
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', {session:false}),
+  (req,res)=>{
+    Profile.findOne( {user: req.user.id} ).then(profile=>{
+        Post.findById(req.params.id)
+          .then(post=>{
+            if( post.likes.filter(like => like.user.toString() === req.user.id).length > 0 ){
+              return res.status(400).json({alreadyliked: 'user already liked this post'});
+            }
+
+            //add user id to likes array
+            post.likes.unshift( {user:req.user.id} );
+
+            post.save().then(post => res.json(post));
+        })
+        .catch(err=>res.status(404).json( {postnotfound: "Post not found"} ));
+      });
+  });
+
+
 module.exports = router;
