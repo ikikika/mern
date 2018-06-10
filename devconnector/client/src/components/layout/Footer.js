4d3c90e0bb3408b3e5f0fb33e9bf839b33d36346
ikikika <iki.jito@gmail.com>
2018-06-10 17:11:54 +0800

    devconn create basic components and render to index page

---
+++ devconnector/client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 t-7">
+      Copyright &copy; {new Date().getFullYear()} DevConnector
+    </footer>
+  )
+}
