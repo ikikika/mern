2368c4c5d754290e2330298993cc16c32ffbe953
ikikika <iki.jito@gmail.com>
2018-06-20 11:00:37 +0800

    devconn signup form onSubmit bind to inputs and console log

--- devconnector/client/src/components/auth/Register.js
+++ devconnector/client/src/components/auth/Register.js
@@ -12,10 +12,22 @@ class Register extends Component {
     };
 
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e){
     this.setState({[e.target.name]: e.target.value });
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+    console.log(newUser);
   }
 
   render() {
@@ -26,7 +38,7 @@ class Register extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
               <p className="lead text-center">Create your DevConnector account</p>
-              <form action="create-profile.html">
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
