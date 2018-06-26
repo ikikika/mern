0d255bd1f905f94d45a5517cfa5e757bdf6313a9
ikikika <iki.jito@gmail.com>
2018-06-26 17:29:27 +0800

    devcon login, use npm i jwt-decode to decode token in redux, dispatch and catch in reducer

--- devconnector/client/src/actions/authActions.js
+++ devconnector/client/src/actions/authActions.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+
 import { GET_ERRORS } from './types';
 
 //register user
@@ -13,3 +17,35 @@ export const registeruser = (userData, history) => dispatch => {
       })
     );
 };
+
+
+//login get user token
+export const loginUser = (userData) => dispatch => {
+  axios
+  .post('/api/users/login', userData)
+  .then(res=>{
+      //save to local storage
+      const { token } = res.data;
+      // set token to local storage
+      localStorage.setItem('jwtToken', token);
+      // set token to auth header
+      setAuthToken(token);
+      //decode token to get user data
+      const decoded = jwt_decode(token);
+      //set current user
+      dispatch(setCurrentUser(decoded));
+  })
+  .catch(err=>
+    dispatch({
+
+    })
+  )
+};
+
+//set logged in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
