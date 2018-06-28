cb08ec527ad26ae29f16fe158173913186d47c35
ikikika <iki.jito@gmail.com>
2018-06-28 11:40:32 +0800

    devcon logout, conditional navbar links, get rid of localstorage data

--- devconnector/client/src/actions/authActions.js
+++ devconnector/client/src/actions/authActions.js
@@ -51,3 +51,15 @@ export const setCurrentUser = (decoded) => {
     payload: decoded
   }
 }
+
+// log user out
+export const logoutUser = () => dispatch => {
+  //remove token from lcoalstorage
+  localStorage.removeItem('jwtToken');
+  //remove auth header for future requests
+  setAuthToken(false);
+  //set current user to {} which will set isAuthenticated to false
+  dispatch(
+    setCurrentUser({})
+  );
+}
