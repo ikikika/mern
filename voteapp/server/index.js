66891c556fdf855f30b04ebde33f20adf5ef839a
ikikika <iki.jito@gmail.com>
2019-07-26 21:07:38 +0800

    set up error handler

--- voteapp/server/index.js
+++ voteapp/server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const handle = require("./handlers");
+
 const app = express();
 const port = 4000;
 
@@ -7,17 +9,7 @@ app.get("/", (req, res) => {
   res.send("hello 123");
 });
 
-app.use((req, res, next) => {
-  const err = new Err("Not found");
-  err.status = 404;
-
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    err: err.message || "SOmething went wrong"
-  });
-});
+app.use(handle.notFound);
+app.use(handle.errors);
 
 app.listen(port, console.log(`Server started on port ${port}`));
