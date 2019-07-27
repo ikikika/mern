df27fc3484847882193b21039c55aadd4a7d0b6b
ikikika <iki.jito@gmail.com>
2019-07-28 00:27:21 +0800

    set up mongodb

--- voteapp/server/index.js
+++ voteapp/server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyparser = require("body-parser");
 
+const db = require("./models");
 const handle = require("./handlers");
 
 const app = express();
