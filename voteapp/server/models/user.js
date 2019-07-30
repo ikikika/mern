b2939ccf067eb1cd793602f05ec1718714640508
ikikika <iki.jito@gmail.com>
2019-07-30 20:39:13 +0800

    encrypt password

--- voteapp/server/models/user.js
+++ voteapp/server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -16,5 +17,26 @@ const userSchema = new mongoose.Schema({
   },
   polls: [{ type: mongoose.Schema.Types.ObjectId, ref: "Poll" }]
 });
+
+userSchema.pre("save", async function(next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+    const hashed = await bcrypt.hash(this.password, 10);
+    this.password = hashed;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+userSchema.methods.comparePassword = async function(attempt, next) {
+  try {
+    return await bcrypt.compare(attempt, this.password);
+  } catch (err) {
+    return next(err);
+  }
+};
 
 module.exports = mongoose.model("User", userSchema);
