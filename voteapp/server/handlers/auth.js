b4db2d8a2fc13d0d825f6743436bf878af771bbd
ikikika <iki.jito@gmail.com>
2019-08-01 23:09:53 +0800

    login function

--- voteapp/server/handlers/auth.js
+++ voteapp/server/handlers/auth.js
@@ -3,7 +3,11 @@ const db = require("../models");
 exports.register = async (req, res, next) => {
   try {
     const user = await db.User.create(req.body);
-    res.json(user);
+    const { id, username } = user;
+    res.json({
+      id,
+      username
+    });
   } catch (err) {
     return next(err);
   }
@@ -11,6 +15,18 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
+    const user = await db.User.findOne({ username: req.body.username });
+    const { id, username } = user;
+    const valid = await user.comparePassword(req.body.password);
+
+    if (valid) {
+      res.json({
+        id,
+        username
+      });
+    } else {
+      throw new Error("Invalid username or password");
+    }
   } catch (err) {
     return next(err);
   }
