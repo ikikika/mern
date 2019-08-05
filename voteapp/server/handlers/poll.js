239814cb3cb6e0a4781a5cdbb2de11c7d50b0184
ikikika <iki.jito@gmail.com>
2019-08-05 23:20:14 +0800

    create poll, post question and option to db

--- voteapp/server/handlers/poll.js
+++ voteapp/server/handlers/poll.js
@@ -9,3 +9,17 @@ exports.showPolls = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.createPoll = async (req, res, next) => {
+  try {
+    const { question, options } = req.body;
+    const poll = await db.Poll.create({
+      question,
+      options: options.map(option => ({ option, votes: 0 }))
+    });
+    res.status(201).json(poll);
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
+};
--- voteapp/server/routes/poll.js
+++ voteapp/server/routes/poll.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 
 const handle = require("../handlers");
 
-router.route("/").get(handle.showPolls);
+router
+  .route("/")
+  .get(handle.showPolls)
+  .post(handle.createPoll);
 
 module.exports = router;
