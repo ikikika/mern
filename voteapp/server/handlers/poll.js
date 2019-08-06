999511a7d9d44dc691dba3f2ae3c299c251f3a23
ikikika <iki.jito@gmail.com>
2019-08-06 20:28:19 +0800

    add poll creator

--- voteapp/server/handlers/poll.js
+++ voteapp/server/handlers/poll.js
@@ -12,12 +12,18 @@ exports.showPolls = async (req, res, next) => {
 
 exports.createPoll = async (req, res, next) => {
   try {
+    const { id } = req.decoded;
+    const user = await db.User.findById(id);
+
     const { question, options } = req.body;
     const poll = await db.Poll.create({
       question,
+      user,
       options: options.map(option => ({ option, votes: 0 }))
     });
-    res.status(201).json(poll);
+    user.polls.push(poll._id);
+
+    res.status(201).json({ ...poll._doc, user: user._id });
   } catch (err) {
     err.status = 400;
     next(err);
--- voteapp/server/routes/poll.js
+++ voteapp/server/routes/poll.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 
 const handle = require("../handlers");
+const auth = require("../middlewares/auth");
 
 router
   .route("/")
   .get(handle.showPolls)
-  .post(handle.createPoll);
+  .post(auth, handle.createPoll);
 
 module.exports = router;
