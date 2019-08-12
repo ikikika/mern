a52dd0eaf15f5fbc6d5a9956519d00f48dba4ca2
ikikika <iki.jito@gmail.com>
2019-08-13 02:43:34 +0800

    create api service, test with hardcoded login

--- voteapp/client/src/components/App.jsx
+++ voteapp/client/src/components/App.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => <div>App Works</div>;
+import api from "../services/api";
+
+// const App = () => <div>App Works</div>;
+
+class App extends Component {
+  async componentDidMount() {
+    const result = await api.call("post", "auth/login", {
+      username: "user1",
+      password: "password"
+    });
+
+    console.log(result);
+  }
+  render() {
+    return <div>App Work111s</div>;
+  }
+}
 
 export default App;
