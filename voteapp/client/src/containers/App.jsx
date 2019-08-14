ea5b496541fc513ae843d3012766d75178de5516
ikikika <iki.jito@gmail.com>
2019-08-15 05:21:01 +0800

    set up auth reducer

--- voteapp/client/src/containers/App.jsx
+++ voteapp/client/src/containers/App.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
+import decode from "jwt-decode";
 
-import api from "../services/api";
 import { store } from "../store";
+import { setCurrentUser, addError, setToken } from "../store/actions";
+
+if (localStorage.jwtToken) {
+  setToken(localStorage.jwtToken);
+  try {
+    store.dispatch(setCurrentUser(decode(localStorage.jwtToken)));
+  } catch (err) {
+    store.dispatch(setCurrentUser({}));
+    store.dispatch(addError(err));
+  }
+}
 
 const App = () => (
   <Provider store={store}>
