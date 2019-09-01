b401620ad8e450a44b23b06ea95cb755e40b57d5
ikikika <iki.jito@gmail.com>
2019-09-02 06:01:19 +0800

    create poll page and finish app logic

--- voteapp/client/src/containers/RouteViews.jsx
+++ voteapp/client/src/containers/RouteViews.jsx
@@ -8,6 +8,7 @@ import AuthPage from "../pages/AuthPage";
 import TestPage from "../pages/TestPage";
 import Homepage from "../pages/Homepage";
 import PollPage from "../pages/PollPage";
+import CreatePollPage from "../pages/CreatePollPage";
 
 const RouteViews = ({ auth, getCurrentPoll }) => (
   <main>
@@ -28,6 +29,11 @@ const RouteViews = ({ auth, getCurrentPoll }) => (
             isAuthenticated={auth.isAuthenticated}
           />
         )}
+      />
+      <Route
+        exact
+        path="/poll/new"
+        render={() => <CreatePollPage isAuthenticated={auth.isAuthenticated} />}
       />
       <Route
         exact
