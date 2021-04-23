d03ad11efb9b3e46ea355e429ceaa46dbba3e769
ikikika <iki.jito@gmail.com>
2021-04-23 14:56:14 +0800

    user login reducer & action

--- ecomm/frontend/src/store.js
+++ ecomm/frontend/src/store.js
@@ -6,18 +6,29 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
+// cart initialisation
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
-const initialState = { cart: { cartItems: cartItemsFromStorage } };
+// userinfo initialisation
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 const middleware = [thunk];
 
